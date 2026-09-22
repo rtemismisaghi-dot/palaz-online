7217ff2f4fde865e4381793cc2429304e193c505
rtemismisaghi-dot <rtemis.misaghi@gmail.com>
2026-09-22 15:46:53 +0330

    fix: seed storefront directly without duplicate seeder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 php artisan migrate --force
-php artisan db:seed --class='App\\Seeders\\DatabaseSeeder' --force
+php artisan db:seed --class='App\\Seeders\\StoreCatalogSeeder' --force
 exec apache2-foreground

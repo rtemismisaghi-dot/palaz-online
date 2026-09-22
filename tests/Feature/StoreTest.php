6a2f14568d78fecdf7cf182f7e8c1ce6f3b0f63b
rtemismisaghi-dot <rtemis.misaghi@gmail.com>
2026-09-22 14:37:12 +0330

    test: cover storefront flows

---
+++ tests/Feature/StoreTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+    public function test_home_page_loads(): void
+    {
+        $this->get('/')->assertOk()->assertSee('PALAZ ONLINE')->assertSee('دسته‌بندی‌ها');
+    }
+
+    public function test_shop_can_filter_by_category(): void
+    {
+        $this->get('/shop?category=carpet')
+            ->assertOk()
+            ->assertSee('موکت پالاز مدل آرتا')
+            ->assertDontSee('لمینیت پالاز مدل کلاسیک');
+    }
+
+    public function test_product_can_be_added_to_cart(): void
+    {
+        $this->post('/cart/add/carpet-arta')
+            ->assertRedirect('/cart');
+
+        $this->get('/cart')->assertOk()->assertSee('موکت پالاز مدل آرتا');
+    }
+
+    public function test_service_request_is_stored_in_session(): void
+    {
+        $this->post('/services/request', [
+            'type' => 'measurement',
+            'name' => 'تست کاربر',
+            'phone' => '09120000000',
+            'description' => 'تست اندازه‌گیری',
+        ])->assertRedirect();
+
+        $this->get('/')->assertOk()->assertSee('PALAZ ONLINE');
+    }
+}

b294d61f795c27a892d61d1bf82eb3cf64b13b24
rtemismisaghi-dot <rtemis.misaghi@gmail.com>
2026-09-22 15:41:38 +0330

    fix: use app autoloaded database seeder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 php artisan migrate --force
-php artisan db:seed --class='Database\\Seeders\\DatabaseSeeder' --force
+php artisan db:seed --class='App\\Seeders\\DatabaseSeeder' --force
 exec apache2-foreground

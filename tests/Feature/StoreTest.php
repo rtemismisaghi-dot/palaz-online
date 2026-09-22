ca21b827842580d2026932994469ff10892d6e24
rtemismisaghi-dot <rtemis.misaghi@gmail.com>
2026-09-22 14:49:25 +0330

    test: run storefront feature tests against seeded database

--- tests/Feature/StoreTest.php
+++ tests/Feature/StoreTest.php
@@ -2,10 +2,20 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\StoreCatalogSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class StoreTest extends TestCase
 {
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(StoreCatalogSeeder::class);
+    }
+
     public function test_home_page_loads(): void
     {
         $this->get('/')->assertOk()->assertSee('PALAZ ONLINE')->assertSee('دسته‌بندی‌ها');
@@ -16,7 +26,7 @@ class StoreTest extends TestCase
         $this->get('/shop?category=carpet')
             ->assertOk()
             ->assertSee('موکت پالاز مدل آرتا')
-            ->assertDontSee('لمینیت پالاز مدل کلاسیک');
+            ->assertDontSee('لمینیت پالاز مدل Classic');
     }
 
     public function test_product_can_be_added_to_cart(): void

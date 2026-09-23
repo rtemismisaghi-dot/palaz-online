7e2caa57302e96e8da746a8301f5b3c1896007b6
rtemismisaghi-dot <rtemis.misaghi@gmail.com>
2026-09-23 13:25:24 +0330

    Show full Palaz Online wordmark beside P logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,8 @@
         <button class="mobile-menu" type="button" aria-label="باز کردن منو" aria-expanded="false">☰</button>
 
         <a href="{{ route('home') }}" class="brand brand-real" aria-label="پالاز آنلاین">
-            <img class="brand-logo" src="{{ asset("images/palaz-logo.png") }}" alt="پالاز">
+            <img class="brand-logo" src="{{ asset("images/palaz-logo.png") }}" alt="">
+            <span class="brand-wordmark"><strong>PALAZ</strong><small>ONLINE</small></span>
         </a>
 
         <div class="search search-real">
